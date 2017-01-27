5551eb3f1342c5d44795e1f8700ac37c95a64ca7
alexshtern <alexshtern@users.noreply.github.com>
2017-01-27 13:45:41 +0300

    расширен диалог с пользователем

--- bars.py
+++ bars.py
@@ -1,10 +1,13 @@
+import sys
 import json
 import math
 
 
 def load_data(filepath):
-    with open(filepath, 'r') as bars_file:
-        bars = json.load(bars_file)
+    try:
+        bars = json.load(open(filepath, 'r'))
+    except:
+        bars = None
     return bars
 
 
@@ -43,9 +46,21 @@ def get_closest_bar(bars, longitude, latitude):
 
 
 if __name__ == '__main__':
-    bars = load_data("../data-2897-2016-11-23.json")
+    print("Программа определяет самый большой, самый маленький и самый близкий бар.")
+    while(True):
+        bars_file = input("Введите имя файла с данными о барах: ")
+        print("Чтение информации из файла %s ..." % bars_file)
+        bars = load_data(bars_file)
+        if bars is None:
+            user_input = input("Прочитать данные не удалось. Повторить ввод имени файла? (y/n) ")
+            if user_input.strip().lower() != "y":
+                print("Выход из программы.")
+                sys.exit()
+        else:
+            break
     print("Cамый большой бар: %s" % get_biggest_bar(bars))
     print("Cамый маленький бар: %s" % get_smallest_bar(bars))
-    longitude = float(input("Долгота: ").strip())
-    latitude = float(input("Широта: ").strip())
+    print("Определение ближайшего бара.")
+    longitude = float(input("    Введите вашу текущую долготу: ").strip())
+    latitude = float(input("    Введите вашу текущую широту: ").strip())
     print("Cамый близкий бар: %s" % get_closest_bar(bars, longitude, latitude))
