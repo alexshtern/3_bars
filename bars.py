f96554c6b4fcc06c5cb5bc36716449a03ba3b351
alexshtern <alexshtern@users.noreply.github.com>
2017-01-27 13:11:29 +0300

    изменены названия переменных на более осмысленные

--- bars.py
+++ bars.py
@@ -3,37 +3,37 @@ import math
 
 
 def load_data(filepath):
-    with open(filepath, 'r') as fp:
-        data = json.load(fp)
-    return data
+    with open(filepath, 'r') as bars_file:
+        bars = json.load(bars_file)
+    return bars
 
 
-def get_biggest_bar(data):
+def get_biggest_bar(bars):
     biggest_bar = None
     max_seats_count = 0
-    for bar in data:
+    for bar in bars:
         if int(bar["SeatsCount"]) > max_seats_count:
             max_seats_count = int(bar["SeatsCount"])
             biggest_bar = bar["Name"]
     return biggest_bar
 
 
-def get_smallest_bar(data):
+def get_smallest_bar(bars):
     smallest_bar = None
     min_seats_count = float("inf")
-    for bar in data:
+    for bar in bars:
         if int(bar["SeatsCount"]) < min_seats_count:
             min_seats_count = int(bar["SeatsCount"])
             smallest_bar = bar["Name"]
     return smallest_bar
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_closest_bar(bars, longitude, latitude):
     # Принимаем, что в пределах Мщсквы поверхность Земли плоская.
     # Тогда долгота и широту можно использовать, как декартовы координаты
     closest_bar = None
     min_distance = float("inf")
-    for bar in data:
+    for bar in bars:
         bar_longitude, bar_latitude = bar["geoData"]["coordinates"]
         distance = math.sqrt((longitude - bar_longitude) ** 2 + (latitude - bar_latitude) ** 2)
         if distance < min_distance:
@@ -48,5 +48,4 @@ if __name__ == '__main__':
     print("Cамый маленький бар: %s" % get_smallest_bar(bars))
     longitude = float(input("Долгота: ").strip())
     latitude = float(input("Широта: ").strip())
-    # print("Cамый близкий бар: %s" % get_closest_bar(bars, 37.62158794615201, 55.76536695660836))
     print("Cамый близкий бар: %s" % get_closest_bar(bars, longitude, latitude))
